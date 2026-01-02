530e22ccb23ed7ffed47ec0fd54fe0e9f9b8c7f6
Jeremy <jeremy@rebar.is>
2026-01-02 10:28:05 -0600

    sm

--- affidavit/vite.config.ts
+++ affidavit/vite.config.ts
@@ -1,5 +1,4 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -7,7 +6,6 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
-      presets: [vercelPreset()],
     }),
     tsconfigPaths(),
   ],
